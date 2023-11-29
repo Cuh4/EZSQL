8e6b749991b6a3aa54f6f660287bc805e118739c
Cuh4 <roozzguy@gmail.com>
2023-11-29 20:20:27 +0000

    add keyword args + convert to using tbl methods

--- src/test.py
+++ src/test.py
@@ -10,21 +10,21 @@ import time
 # // SQL Table Definitions
 class food:
     id = EZSQL.column(
-        EZSQL.dataTypes.integerType,
-        0,
-        True
+        type = EZSQL.dataTypes.integerType,
+        default = 0,
+        isPrimary = True
     )
 
     name = EZSQL.column(
-        EZSQL.dataTypes.stringType,
-        "",
-        False
+        type = EZSQL.dataTypes.stringType,
+        default = "",
+        isPrimary = False
     )
 
     description = EZSQL.column(
-        EZSQL.dataTypes.stringType,
-        "",
-        False
+        type = EZSQL.dataTypes.stringType,
+        default = "",
+        isPrimary = False
     )
     
     # so we can create a value at any time. id is optional because id is the primary key
@@ -44,18 +44,14 @@ foodTable = database.createTable("Food", food)
 
 # // Modify Database
 # insert food into food table
-database.insert(
-    table = foodTable, 
-
+foodTable.insert(
     value = food( # since id is the primary key AND its an integer, we don't need to specify it because it will automatically increment
         name = "Chicken",
         description = "It's excellent"
     )
 )
 
-database.insert(
-    table = foodTable, 
-
+foodTable.insert(
     value = food(
         id = 2, # if a food of this id already exists in the db, then this will not be added
         name = "Rice",
@@ -64,11 +60,13 @@ database.insert(
 )
 
 # retrieve something from the db
-foods: list[food] = database.get(
-    table = foodTable, 
+foods: list[food] = foodTable.get(
     searchParameters = food(name = "Rice", description = "h"), # search for food with the name "Rice" and description "h"
     fetchHowMany = -1 # -1 = get all results, 1 = get a singular result (not in a list), 2+ = get x results (in a list)
 )
+
+# retrieve everything from the db
+foods: list[food] = foodTable.getAll()
 
 # foods[0]:
 """
@@ -80,15 +78,14 @@ food(
 """
 
 # remove every value with the name "Chicken" from the database
-database.remove(
-    table = foodTable, 
+foodTable.remove(
     searchParameters = food(name = "Chicken")
 )
 
 # remove all values after 5 seconds
 time.sleep(5)
-database.removeAllValues(foodTable)
+foodTable.removeAll()
 
 # remove table after another 5 seconds
 time.sleep(5)
-database.removeTable(foodTable)
+foodTable.removeTable()
