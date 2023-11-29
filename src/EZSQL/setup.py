6687b4a1934be08b2a7614d436c826de2f75ec9c
Cuh4 <roozzguy@gmail.com>
2023-11-29 15:48:49 +0000

    setup.py for package distribution

---
+++ src/EZSQL/setup.py
@@ -0,0 +1,37 @@
+# // ---------------------------------------------------------------------
+# // ------- [EZSQL] EZSQL Setup
+# // ---------------------------------------------------------------------
+
+# // ---- Imports
+from setuptools import setup, find_packages
+
+# // ---- Variables
+VERSION = "0.0.1"
+DESCRIPTION = "A Python package that makes working with SQLite easier."
+LONG_DESCRIPTION = "A Python package that makes working with SQLite easier by removing the need to use SQL syntax."
+
+# // ---- Main
+setup(
+    name = "EZSQL",
+    version = "0.0.1",
+    author = "Cuh4",
+
+    description = "A Python package that makes working with SQLite easier.",
+    long_description = "A Python package that makes working with SQLite easier by removing the need to use SQL syntax.",
+    
+    packages = find_packages(),
+    install_requires = [
+        "os",
+        "sqlite3",
+        "pathlib"
+    ],
+    
+    keywords = [
+        "SQL",
+        "SQLite",
+        "Python Package"
+    ]
+)
+
+# py setup.py sdist bdist_wheel
+# twine upload dist/*
